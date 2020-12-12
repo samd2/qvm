f7a4c30511acc60d271b040e51daccc6ea470b00
zajo <emildotchevski@gmail.com>
2020-12-11 23:16:03 -0800

    testing

--- test/msvc_test.cpp
+++ test/msvc_test.cpp
@@ -266,7 +266,7 @@ boost
         namespace
         qvm_detail
             {
-            template <class A,bool M=is_mat<A>::value,bool Q=is_quat<A>::value,bool V=is_vec<A>::value>
+            template <class A,bool M=is_mat<A>::value,bool Q=is_quat<A>::value,bool V=is_vec<A>::value,bool S=is_scalar<A>::value>
             struct
             scalar_impl
                 {
@@ -274,23 +274,30 @@ boost
 
             template <class A>
             struct
-            scalar_impl<A,true,false,false>
+            scalar_impl<A,true,false,false,false>
                 {
                 typedef typename mat_traits<A>::scalar_type type;
                 };
 
             template <class A>
             struct
-            scalar_impl<A,false,true,false>
+            scalar_impl<A,false,true,false,false>
                 {
                 typedef typename quat_traits<A>::scalar_type type;
                 };
 
             template <class A>
             struct
-            scalar_impl<A,false,false,true>
+            scalar_impl<A,false,false,true,false>
                 {
                 typedef typename vec_traits<A>::scalar_type type;
+                };
+
+            template <class S>
+            struct
+            scalar_impl<S,false,false,false,true>
+                {
+                typedef S type;
                 };
             }
 
@@ -325,19 +332,33 @@ boost
             {
             template <class A,class B,
                 bool QA=is_quat<A>::value,
-                bool QB=is_quat<B>::value>
+                bool QB=is_quat<B>::value,
+                class SA=typename scalar<A>::type,
+                class SB=typename scalar<B>::type,
+                class SR=typename deduce_scalar<typename scalar<A>::type,typename scalar<B>::type>::type>
             struct
-            deduce_quat2_default
+            deduce_q2_default
                 {
-                typedef quat<
-                    typename deduce_scalar<
-                        typename scalar<A>::type,
-                        typename scalar<B>::type>::type> type;
+                typedef quat<SR> type;
                 };
 
-            template <class Q>
+            template <class Q,class S>
             struct
-            deduce_quat2_default<Q,Q,true,true>
+            deduce_q2_default<Q,Q,true,true,S,S,S>
+                {
+                typedef Q type;
+                };
+
+            template <class Q,class S,class SV>
+            struct
+            deduce_q2_default<Q,S,true,false,SV,S,SV>
+                {
+                typedef Q type;
+                };
+
+            template <class S,class Q,class SV>
+            struct
+            deduce_q2_default<S,Q,false,true,S,SV,SV>
                 {
                 typedef Q type;
                 };
@@ -347,7 +368,7 @@ boost
         struct
         deduce_quat2
             {
-            typedef typename qvm_detail::deduce_quat2_default<A,B>::type type;
+            typedef typename qvm_detail::deduce_q2_default<A,B>::type type;
             };
         }
     }
@@ -412,10 +433,10 @@ boost
         template <class A,class B>
         typename lazy_enable_if_c<
             is_quat<A>::value && is_scalar<B>::value,
-            deduce_quat<A> >::type
+            deduce_quat2<A,B> >::type
         operator*( A const & a, B b )
             {
-            typedef typename deduce_quat<A>::type R;
+            typedef typename deduce_quat2<A,B>::type R;
             R r;
             quat_traits<R>::template write_element<0>(r)=quat_traits<A>::template read_element<0>(a)*b;
             quat_traits<R>::template write_element<1>(r)=quat_traits<A>::template read_element<1>(a)*b;
